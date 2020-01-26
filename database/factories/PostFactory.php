bd04873a8106f06a13e4003d80e59491f26674c9
Shpakouski <job.shpakouski@gmail.com>
2020-01-27 02:39:35 +0300

    added factories and seeders

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Post;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+$factory->define(Post::class, function (Faker $faker) {
+    $title=$faker->realText(rand(10, 40));
+    return [
+        'title' => $title,
+        'author_id' => factory(App\User::class),
+        'short_title' => Str::length($title) > 30 ? Str::substr($title, 0, 30) . '...' : $title,
+        'description' =>$faker->realText(rand(100, 500)),
+
+    ];
+});
