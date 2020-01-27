799739dfa87e135d73c242a7f30066ebb56472ba
Shpakouski <job.shpakouski@gmail.com>
2020-01-27 03:55:36 +0300

    modified index.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -44,5 +44,20 @@
         </form>
     </div>
 </nav>
+
+<div class="container">
+    <div class="row">
+@foreach($posts as $post)
+        <div class="col-6">
+            <div class="card">
+                <div class="card-header">{{$post->short_title}}</div>
+                <card-body>{{$post->description}}</card-body>
+            </div>
+        </div>
+@endforeach
+    </div>
+</div>
+
+
 </body>
 </html>
