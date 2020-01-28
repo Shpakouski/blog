3186314090bbab177f8183bf93f7d4a6f60b7c09
Shpakouski <job.shpakouski@gmail.com>
2020-01-28 04:09:09 +0300

    added validation and russian language localization

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PostRequest;
 use App\Post;
 use App\User;
 use Illuminate\Http\Request;
@@ -55,7 +56,7 @@ class PostController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(PostRequest $request)
     {
         $post = new Post();
         $post->title=$request->title;
@@ -104,7 +105,7 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(PostRequest $request, $id)
     {
         $post = Post::find($id);
         $post->title=$request->title;
--- resources/views/posts/parts/form.blade.php
+++ resources/views/posts/parts/form.blade.php
@@ -1,8 +1,8 @@
 <div class="form-group">
-    <input type="text" class="form-control" name="title"value="{{$post->title ?? ''}}" required>
+    <input type="text" class="form-control" name="title"value="{{old('title') ?? $post->title ?? ''}}" required>
 </div>
 <div class="form-group">
-    <textarea name="description" rows="10" class="form-control" required>{{$post->description ?? ''}}</textarea>
+    <textarea name="description" rows="10" class="form-control" required>{{old('description') ?? $post->description ?? ''}}</textarea>
 </div>
 <div class="form-group">
     <input type="file" name="img">
